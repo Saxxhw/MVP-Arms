b1853747c26a481a2b8faec77a6a5507791fbd24
Saxxhw <wang921018>
2021-01-27 10:49:32 +0800

    Initial commit

---
+++ app/src/main/java/com/zkyc/arms/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.zkyc.arms
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
